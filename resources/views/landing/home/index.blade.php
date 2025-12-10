1c6e6dad28b9c0c17a04f9e184409860043c5cf9
Devsam_ <sabillyamri1@gmail.com>
2025-12-10 20:17:01 +0700

    first commit

---
+++ resources/views/landing/home/index.blade.php
@@ -0,0 +1,19 @@
+@extends('landing.layouts.app', [
+    'title' => 'Berbinar Insightful Indonesia',
+])
+
+@section('content')
+    <div class="min-h-screen flex justify-center items-center bg-blue-300">
+        <div class="text-center">
+            <h1 class="text-8xl font-bold text-black text-white mb-4">
+                Hello World
+            </h1>
+            <p class="text-xl text-black opacity-90">
+                Welcome to E-Learning Platform
+            </p>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+@endsection
---
+++ resources/views/landing/partials/header.blade.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>Education</title>
+
+<link rel="shortcut icon" href="{{ asset("") }}" type="image/x-icon" />
+@vite("resources/css/app.css")
